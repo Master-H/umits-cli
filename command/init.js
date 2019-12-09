f6d1f76351c38b1b3a144e8c26a86514def6fc88
hxy <huangxingyuan@didiglobal.com>
2019-12-09 19:16:27 +0800

    delete git

--- command/init.js
+++ command/init.js
@@ -23,7 +23,6 @@ module.exports = () => {
 	// gitUrl = config.tpl[tplName].url
 	branch = 'master'
 	// branch = config.tpl[tplName].branch
-
     // git命令，远程拉取项目并自定义项目名
     // let cmdStr = `git clone --depth=1 ${gitUrl} ${projectName}  && cd ${projectName} `
     let cmdStr = `git clone --depth=1 ${gitUrl} ${projectName}  && cd ${projectName} && git checkout ${branch}`
@@ -33,18 +32,25 @@ module.exports = () => {
 	console.log(yosay(chalk.red(`\n 模板下载中 ... \n 模板地址：${gitUrl}`)))
   const spinner = ora(`下载模板，源地址:${gitUrl}`)
   // spinner.start()
-	exec(cmdStr, (error, stdout, stderr) => {
+	exec(cmdStr, (error) => {
       if (error) {
         console.log(error)
-        // spinner.fail()
         process.exit()
       }
+      exec('cd ' + projectName + ' && rm -rf .git', (err, out) => {
+        if(err){
+          console.log(err);
+          tip.fail('请重新运行!');
+          process.exit();
+        }
+        console.log(chalk.green('\n √  completed!'))
+        console.log(chalk.red(`\n cd ${projectName} && npm install \n`))
+        console.log(chalk.red(`\n npm start`))
+        process.exit()
+      });
       // spinner.succeed()
       // console.log(yosay(chalk.red(`\n 下载完成 ... `)))
-      console.log(chalk.green('\n √  completed!'))
-      console.log(chalk.red(`\n cd ${projectName} && npm install \n`))
-      console.log(chalk.red(`\n npm start`))
-      process.exit()
+     
 	})
   })
 }
