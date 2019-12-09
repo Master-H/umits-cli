370b8ae9ff85dcf3eedadcd2138ff99359d1c05b
hxy <huangxingyuan@didiglobal.com>
2019-12-09 19:57:35 +0800

    test2

--- command/init.js
+++ command/init.js
@@ -12,35 +12,31 @@ module.exports = () => {
     // 处理用户输入
   	// let tplName = yield prompt('Template name: ')
   	let projectName = yield prompt('Project name: ')
-  	let gitUrl
-  	let branch
-    gitUrl = 'git@github.com:Master-H/dvats.git'
-    branch = 'master'
-    console.log(__dirname);
-    
-  // let cmdStr = `git clone --depth=1 ${gitUrl} ${projectName}  && cd ${projectName} && git checkout ${branch}`
-	// console.log(yosay(chalk.red(`\n 模板下载中 ... \n 模板地址：${gitUrl}`)))
-  // // const spinner = ora(`下载模板，源地址:${gitUrl}`)
-  // // spinner.start()
-	// exec(cmdStr, {cwd:},(error) => {
-  //     if (error) {
-  //       console.log(error)
-  //       process.exit()
-  //     }
-  //     exec('cd ' + projectName + ' && rm -rf .git', (err, out) => {
-  //       if(err){
-  //         console.log(err);
-  //         tip.fail('请重新运行!');
-  //         process.exit();
-  //       }
-  //       console.log(chalk.green('\n √  completed!'))
-  //       console.log(chalk.red(`\n cd ${projectName} && npm install \n`))
-  //       console.log(chalk.red(`\n npm start`))
-  //       process.exit()
-  //     });
-  //     // spinner.succeed()
-  //     // console.log(yosay(chalk.red(`\n 下载完成 ... `)))
+  	let gitUrl = 'git@github.com:Master-H/dvats.git'
+    let branch  = 'master'
+    let currentPath = process.cwd()
+  let cmdStr = `git clone --depth=1 ${gitUrl} ${projectName}  && cd ${projectName} && rm -rf .git`
+	console.log(yosay(chalk.red(`\n 模板下载中 ... \n 模板地址：${gitUrl}`)))
+	exec(cmdStr, {cwd:currentPath},(error) => {
+      if (error) {
+        console.log(error)
+        process.exit()
+      }
+      console.log(chalk.green('\n √  completed!'))
+      console.log(chalk.red(`\n cd ${projectName} && npm install \n`))
+      console.log(chalk.red(`\n npm start`))
+      process.exit()
+      // exec('cd ' + projectName + ' && rm -rf .git', (err, out) => {
+      //   if(err){
+      //     console.log(err);
+      //     tip.fail('请重新运行!');
+      //     process.exit();
+      //   }
+       
+      // });
+      // spinner.succeed()
+      // console.log(yosay(chalk.red(`\n 下载完成 ... `)))
      
-	// })
+	})
   })
 }
